87ea734ec51c455c2e84a89b47d826b00513c28a
EpistemeSoft <40729563+EpistemeSoft@users.noreply.github.com>
2018-07-02 18:47:54 -0700

    Update SortedMerge.c

--- SortedMerge.c
+++ SortedMerge.c
@@ -4,97 +4,79 @@
 #include<assert.h>
 
 /* Link list node */
-struct Node
+typedef struct node_s
 {
-	int data;
-	struct Node* next;
-};
+  int data;
+  struct node_s *pNext;
+}node_t;
 
-/* pull off the front node of the source and put it in dest */
-void MoveNode(struct Node** destRef, struct Node** sourceRef);
-
-/* Takes two lists sorted in increasing order, and splices
-their nodes together to make one big sorted list which
-is returned. */
-struct Node* SortedMerge(struct Node* a, struct Node* b)
+node_t* SortedMerge(node_t *a, node_t *b)
 {
-    struct Node z;
-    struct Node *pTemp = &z;
+    node_t z;
+    node_t *pTemp = &z;
     
     while(a != NULL && b != NULL)
     {
         if(a->data <= b->data)
         {
-            pTemp->next = a;
-            pTemp = pTemp->next;
-            a = a->next;
+            pTemp->pNext = a;
+            pTemp = pTemp->pNext;
+            a = a->pNext;
         }
         else
         {
-            pTemp->next = b;
-            pTemp = pTemp->next;
-            b = b->next;
+            pTemp->pNext = b;
+            pTemp = pTemp->pNext;
+            b = b->pNext;
         }
     }
     if(a == NULL)
-      pTemp->next = b;
+      pTemp->pNext = b;
     else
-      pTemp->next = a;
-    return z.next;
+      pTemp->pNext = a;
+    return z.pNext;
 }
  
-/* Function to insert a node at the beginging of the
-linked list */
-void push(struct Node** head_ref, int new_data)
+/* Function to insert a node at the beginging of the linked list */
+void push(node_t  **pHead, int newData)
 {
-	/* allocate node */
-	struct Node* new_node =
-		(struct Node*) malloc(sizeof(struct Node));
-
-	/* put in the data */
-	new_node->data = new_data;
-
-	/* link the old list off the new node */
-	new_node->next = (*head_ref);
-
-	/* move the head to point to the new node */
-	(*head_ref) = new_node;
+  /* allocate node */
+  node_t *newNode = (node_t*)malloc(sizeof(node_t));
+  new_node->data = newData;
+  newNode->pNext = (*pHead);
+  *pHead = newNode;
 }
 
 /* Function to print nodes in a given linked list */
-void printList(struct Node *node)
+void printList(node_t *node)
 {
-	while (node!=NULL)
-	{
-		printf("%d ", node->data);
-		node = node->next;
-	}
+  while (node!=NULL)
+  {
+    printf("%d ", node->data);
+    node = node->pNext;
+  }
 }
 
 /* Drier program to test above functions*/
 int main()
 {
-	/* Start with the empty list */
-	struct Node* res = NULL;
-	struct Node* a = NULL;
-	struct Node* b = NULL;
+  /* Start with the empty list */
+  node_t *res = NULL;
+  node_t *a = NULL;
+  node_t *b = NULL;
+  Created lists, a: 5->10->15, b: 2->3->20 */
+  push(&a, 15);
+  push(&a, 10);
+  push(&a, 5);
+  push(&b, 20);
+  push(&b, 3);
+  push(&b, 2);
 
-	/* Let us create two sorted linked lists to test
-	the functions
-	Created lists, a: 5->10->15, b: 2->3->20 */
-	push(&a, 15);
-	push(&a, 10);
-	push(&a, 5);
+  /* Remove duplicates from linked list */
+  res = SortedMerge(a, b);
 
-	push(&b, 20);
-	push(&b, 3);
-	push(&b, 2);
+  printf("Merged Linked List is: \n");
+  printList(res);
 
-	/* Remove duplicates from linked list */
-	res = SortedMerge(a, b);
-
-	printf("Merged Linked List is: \n");
-	printList(res);
-
-	return 0;
+  return 0;
 }
